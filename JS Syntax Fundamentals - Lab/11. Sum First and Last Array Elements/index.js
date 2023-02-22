f3a3c981d3e234c85c0d4b70c24320eeb8107f60
ZahariBakov <zaharibakov@gmail.com>
2023-02-22 23:57:44 +0200

    Add exercises "11. Sum First and Last Array Elements" and "12. Reverse an Array of Numbers".

---
+++ JS Syntax Fundamentals - Lab/11. Sum First and Last Array Elements/index.js
@@ -0,0 +1,10 @@
+function solve(array) {
+    let firstnum = array[0];
+    let lastNum = array[array.length - 1];
+
+    console.log(firstnum + lastNum);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
