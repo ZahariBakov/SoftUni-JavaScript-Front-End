3881d39a4caba6a0265a4114869ae8448ad9b8a0
ZahariBakov <zaharibakov@gmail.com>
2023-02-22 22:33:12 +0200

    Add exercises "05. Math Operations" and "06. Largest Number".

---
+++ JS Syntax Fundamentals - Lab/06. Largest Number/index.js
@@ -0,0 +1,20 @@
+function solve(firstNum, secondNum, thirdNum) {
+    let result;
+    
+    if (firstNum > secondNum && firstNum > thirdNum) {
+        result = firstNum;
+    }
+
+    else if (secondNum > thirdNum) {
+        result = secondNum
+    }
+
+    else {
+        result = thirdNum
+    }
+
+    console.log(`The largest number is ${result}.`)
+}
+
+solve(5, -3, 16)
+solve(-3, -5, -22.5)
