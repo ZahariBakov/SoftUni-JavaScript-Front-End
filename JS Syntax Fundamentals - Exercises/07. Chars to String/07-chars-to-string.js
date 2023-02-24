162ce626694c128896b9596f5205506496d9f449
ZahariBakov <zaharibakov@gmail.com>
2023-02-24 09:40:30 +0200

    Add exercises "07. Chars to String" and "08. Reversed Chars".

---
+++ JS Syntax Fundamentals - Exercises/07. Chars to String/07-chars-to-string.js
@@ -0,0 +1,23 @@
+function charsToString(first, second, third) {
+    let result = first.concat(second, third);
+
+    console.log(result);
+}
+
+charsToString(
+    'a',
+    'b',
+    'c'
+);
+
+charsToString(
+    '%',
+    '2',
+    'o'
+);
+
+charsToString(
+    '1',
+    '5',
+    'p'
+);
---
+++ JS Syntax Fundamentals - Exercises/08. Reversed Chars/08-reversed-chars.js
@@ -0,0 +1,17 @@
+function reversedChars(first, second, third) {
+    let result = third.concat(' ', second, ' ', first);
+
+    console.log(result);
+}
+
+reversedChars(
+    'A',
+    'B',
+    'C'
+);
+
+reversedChars(
+    '1',
+    'L',
+    '&'     
+);
