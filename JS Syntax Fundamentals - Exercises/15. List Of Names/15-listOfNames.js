265f51d8d54fb96c1278d27d121a19bed5d2ab36
ZahariBakov <zaharibakov@gmail.com>
2023-02-27 17:48:07 +0200

    Add exercises "15. List Of Names" and "16. Sorting Numbers".

---
+++ JS Syntax Fundamentals - Exercises/15. List Of Names/15-listOfNames.js
@@ -0,0 +1,8 @@
+function listOfNames(names) {
+    return [...names]
+        .sort((a, b) => a.localeCompare(b))
+        .map((name, i) => `${i + 1}.${name}`)
+        .join(`\n`);
+}
+
+listOfNames(["John", "Bob", "Christina", "Ema"]);
---
+++ JS Syntax Fundamentals - Exercises/16. Sorting Numbers/16-sortingNumbers.js
@@ -0,0 +1,15 @@
+function sortingNumbers(numbers) {
+    let sortedNumbers = []
+
+    numbers.sort((a, b) => a - b);
+
+    while (numbers.length > 0) {
+        sortedNumbers.push(numbers.shift());
+        sortedNumbers.push(numbers.pop());
+    }
+
+    return sortedNumbers
+}
+    
+
+console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
