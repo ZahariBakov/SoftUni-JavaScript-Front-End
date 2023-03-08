f97adcf882a071303c46f936207c5fa177cdfada
ZahariBakov <zaharibakov@gmail.com>
2023-03-08 18:35:17 +0200

    Update exercise "07.nxNMatrix".

--- Functions and Statements - Exercises/07.nxNMatrix.js
+++ Functions and Statements - Exercises/07.nxNMatrix.js
@@ -1,23 +1,24 @@
-// First decision:
 function nxNMatrix(num) {
-    let matrix  = [];
+    // First decision:
+    // let matrix  = [];
 
-    for (let i = 0; i < num; i++) {
-        let row = [];
-        for (let j = 0; j < num; j++) {
-            row.push(num);
-        }
+    // for (let i = 0; i < num; i++) {
+    //     let row = [];
+    //     for (let j = 0; j < num; j++) {
+    //         row.push(num);
+    //     }
 
-        matrix.push(row);
-    }
+    //     matrix.push(row);
+    // }
 
-    matrix.forEach((row) => console.log(row.join(' ')));
+    // matrix.forEach((row) => console.log(row.join(' ')));
+
+    
+
+    // Second decision:
+    return new Array(num).fill(new Array(num).fill(num)).forEach(row => console.log(row.join(' ')));
 }
 
 nxNMatrix(3);
 nxNMatrix(7);
 nxNMatrix(2);
-
-
-// Second decision:
-// (num) => new Array(num).fill(new Array(num).fill(num)).forEach(row => console.log(row.join(' ')));
