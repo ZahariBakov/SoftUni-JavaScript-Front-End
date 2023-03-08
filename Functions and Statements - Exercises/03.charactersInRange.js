54d07098759debafe15d0031a1d683a0010e406c
ZahariBakov <zaharibakov@gmail.com>
2023-03-08 16:11:54 +0200

    Update exercise "03.charactersInRange".

--- Functions and Statements - Exercises/03.charactersInRange.js
+++ Functions and Statements - Exercises/03.charactersInRange.js
@@ -1,23 +1,18 @@
 function charactersInRange(firstChar, secondChar) {
-    let result = '';
+    let result = [];
+
     let firstCharCode = firstChar.charCodeAt();
     let secondCharCode = secondChar.charCodeAt();
 
-    if (firstCharCode <= secondCharCode) {
-        for (let i = firstCharCode + 1; i < secondCharCode; i++) {
-            result += String.fromCharCode(i); 
-            result += ' ';           
-        }
+    let startingIndex = Math.min(firstCharCode, secondCharCode);
+    let finalIndex = Math.max(firstCharCode, secondCharCode);
+
+
+    for (let i = startingIndex + 1; i < finalIndex; i++) {
+        result.push(String.fromCharCode(i)); 
     }
 
-    else {
-        for (let i = secondCharCode + 1; i < firstCharCode; i++) {
-            result += String.fromCharCode(i); 
-            result += ' ';           
-        }
-    }
-
-    console.log(result)
+    console.log(result.join(' '));
 }
 
 charactersInRange('a', 'd');
