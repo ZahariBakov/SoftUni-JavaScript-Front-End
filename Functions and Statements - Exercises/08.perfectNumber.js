2b5a62b7aece084d485e84d39188016e28722e74
ZahariBakov <zaharibakov@gmail.com>
2023-03-08 17:42:13 +0200

    Add second decision in exercise "08.perfectNumber".

--- Functions and Statements - Exercises/08.perfectNumber.js
+++ Functions and Statements - Exercises/08.perfectNumber.js
@@ -1,10 +1,22 @@
 function perfectNumber(number) {
-    let sum = 1;
+    // First decision:
+    // let sum = 1;
+    // for (let i = 2; i < number; i++) {
+    //     if (number % i == 0) {
+    //         sum += i;
+    //     }
+    // }
+
+    // Second decision:
+    let divisors = [];
+
     for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            sum += i;
+        if (number % i === 0) {
+            divisors.push(i);
         }
     }
+
+    let sum = divisors.reduce((a, b) => {return a + b}, 1)
 
     if (sum == number) {
         console.log('We have a perfect number!');
